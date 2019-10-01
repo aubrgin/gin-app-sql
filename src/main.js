321b81fa2487258a1106fa9d8b23b764ccee85f1
decaruju <julien.decarufel@gmail.com>
2019-10-01 18:52:16 -0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Sql from './components/Sql.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default Sql;
